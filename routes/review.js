4c5a81f0df9e6561097403387218584c02296679
Lakshay1810 <vermalakshay773@gmail.com>
2026-01-16 17:39:14 +0530

    update files

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const wrapAsync = require("../utils/WrapAsync.js");
+const wrapAsync = require("../utils/wrapAsync");
 
 const {
   validateReview,
